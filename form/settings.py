7c6f31f7f93a6f7ea07f1ff99623f1f277cd99f0
nikhitagarg10 <70847019+nikhitagarg10@users.noreply.github.com>
2022-06-08 17:19:35 +0530

    Update settings.py

--- form/settings.py
+++ form/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-q(*7&b9^n)r&o_a9++0=@t^a#*dkv0(=(+x2cr%zcyg&78rg7j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://formintern.herokuapp.com/", "127.0.0.1:8000"]
+ALLOWED_HOSTS = ['*', "https://formintern.herokuapp.com/", "127.0.0.1:8000"]
 
 
 # Application definition
